82b686e895c0e06055a0afcea503007c2b3173af
liucong-Broad-M1 <liucong@broadinstitute.org>
2024-01-01 11:17:32 -0500

    modify update script.

--- update_openwrt.sh
+++ update_openwrt.sh
@@ -1,8 +1,9 @@
 VERSION=$1
 
 if [ "${VERSION}" = "23.05.2" ];
+then
 	echo "The current OpenWRT is running ${VERSION}. Please specify a newer one."
-	exit 0;	
+	exit 0
 else
 	echo "Start upgrading OpenWRT to ${VERSION}."
 	sleep 6
@@ -18,6 +19,8 @@ wget https://downloads.openwrt.org/releases/${VERSION}/targets/x86/64/openwrt-${
 gunzip openwrt-${VERSION}-x86-64-generic-ext4-combined.img.gz
 
 # Copy it to the internal SSD
+echo "The new firmware will be copied to /dev/sda, make sure you understand what you are doing."
+sleep 6
 dd if=openwrt-${VERSION}-x86-64-generic-ext4-combined.img of=/dev/sda bs=4M; sync;
 
 # Resize the partition
